f2987d6d6323430fc88ad550443a881214fdf75f
Duong Pham <duongpham@Duongs-MBP.hub>
2020-03-12 20:50:25 +1100

    split menu option into food ,drink and check out

--- Chinese_menu.rb
+++ Chinese_menu.rb
@@ -32,121 +32,138 @@ food_menu = [
      :ingredient =>"Sweet potatoes, porkchoy, driedn onion, capsicum, baby tomatoes"
     },
 
-    {:name => "Hot and sour crab/chicken/beef soup",
+    {:name => "Hot and sour crab soup",
      :price => 10.99,
-     :ingredient => "Crab/chicken/Beef with sugar, vinegar, in chicken stock with corn flour"
+     :ingredient => "Crab with sugar, vinegar, in chicken stock with corn flour"
     },
 ]
 
 
 drink_menu = [
-    {:type => "Coke",
+    {:name => "Coke",
      :price => 4.50,
     },
 
-    {:type => "Diet-Coke",
+    {:name => "Diet-Coke",
      :price => 4.50,
     },
 
-    {:type => "Orange juice",
+    {:name => "Orange juice",
      :price => 6.50,
     },
 
-    {:type => "Apple juice",
+    {:name => "Apple juice",
       :price => 6.50,
     },
 
-    {:type => "Water",
+    {:name => "Water",
      :price => 3.50,
     },
 
 
 ]
 require 'tty-prompt'
-prompt = TTY::Prompt.new
-menu_choice = prompt.select("What Menu would you like?", ["Place Order", "Exit"])
-if menu_choice == "Place Order"
 
-  food_price = []
-  add_food = "yes"
-  while  add_food == "yes"
-    puts " "
-    puts "Food Menu"
-    puts " "
-    food_menu.each_with_index do |item, i|
-        puts i.to_s + " -  " + item[:name].to_s  
-        puts "Ingredient: " + item[:ingredient] 
-        puts "Price: " + "$" + item[:price].to_s +  " "  
-        puts " "
-        puts " "
-    end
-    puts " "
-    
-    customer_input = -1
-    while customer_input < 0 || customer_input > food_menu.length-1
-      print "What would you like to order?"
-      customer_input = gets.chomp.to_i
-    end
-    puts " "
-    puts food_menu[customer_input][:name] +"  " +"$#{food_menu[customer_input][:price]} " 
-    food_price.push(food_menu[customer_input][:price])
-    puts " "
+order_in_progress = true
 
-    puts " Would you like anything else? "
-    puts " "
-    add_food = gets.chomp
-    
-  end 
+#####
 
+food_price = []
+drink_price = []
 
+while order_in_progress == true
 
-  puts "Would you like to order a drink?"
-  drink_choice = gets.chomp
+  prompt = TTY::Prompt.new
+  menu_choice = prompt.select("What Menu would you like?", ["Food Menu","Drink Menu", "Checkout","Exit"])
 
+  if menu_choice == "Food Menu"
 
-  drink_price = []
-  add_drink = "yes"
-  if drink_choice == "yes"
-    while add_drink == "yes" 
+    add_food = "yes"
+    while  add_food == "yes"
       puts " "
-    puts "=============================================================================="
-    puts "  "
-    puts "Drink Menu"
-    puts " "
-    drink_menu.each_with_index do |item,i|
-      puts i.to_s + " - " + " " + item[:type].to_s + " , $" + item [:price].to_s
+      puts "Food Menu"
+      puts " "
+      food_menu.each_with_index do |item, i|
+          puts i.to_s + " -  " + item[:name].to_s  
+          puts "Ingredient: " + item[:ingredient] 
+          puts "Price: " + "$" + item[:price].to_s +  " "  
+          puts " "
+          puts " "
+      end
       puts " "
       
-    end
-    drink_input = -1
-    while drink_input < 0 || drink_input > drink_menu.length-1
-    print " What would you like to order ? "
-      drink_input = gets.chomp.to_i
-    end
-      puts " "  
-      puts drink_menu[drink_input][:type] + " " + "$#{drink_menu[drink_input][:price]}"
-      drink_price.push(drink_menu[drink_input][:price])
+      customer_input = -1
+      while customer_input < 0 || customer_input > food_menu.length-1
+        print "What would you like to order?"
+        customer_input = gets.chomp.to_i
+      end
       puts " "
+      puts food_menu[customer_input][:name] +"  " +"$#{food_menu[customer_input][:price]} " 
+      food_price.push(food_menu[customer_input][:price])
+      puts " "
+
       puts " Would you like anything else? "
       puts " "
-      add_drink = gets.chomp
-    
+      add_food = gets.chomp
+      
+    end 
+
+  end
+
+  if menu_choice == "Drink Menu"
+
+    puts "Would you like to order a drink?"
+    drink_choice = gets.chomp
+
+    add_drink = "yes"
+    if drink_choice == "yes"
+      while add_drink == "yes" 
+        puts " "
+      puts "=============================================================================="
+      puts "  "
+      puts "Drink Menu"
+      puts " "
+      drink_menu.each_with_index do |item,i|
+        puts i.to_s + " - " + " " + item[:name].to_s + " , $" + item [:price].to_s
+        puts " "
+        
+      end
+      drink_input = -1
+      while drink_input < 0 || drink_input > drink_menu.length-1
+      print " What would you like to order ? "
+        drink_input = gets.chomp.to_i
+      end
+        puts " "  
+        puts drink_menu[drink_input][:name] + " " + "$#{drink_menu[drink_input][:price]}"
+        drink_price.push(drink_menu[drink_input][:price])
+        puts " "
+        puts " Would you like anything else? "
+        puts " "
+        add_drink = gets.chomp
+      
+      end
     end
   end
 
 
-  total_food_price = 0
-  food_price.each {|x| total_food_price += x }
+  if menu_choice == "Checkout"
+    total_food_price = 0
+    food_price.each {|x| total_food_price += x }
 
-  total_drink_price = 0
-  drink_price.each{|y| total_drink_price += y}
+    total_drink_price = 0
+    drink_price.each{|y| total_drink_price += y}
 
-  total = total_food_price + total_drink_price
-  puts " "
-  puts " Your total price is : " + "$"+ total.to_s
-  puts " "
-    
-elsif menu_choice == "Exit"
-  exit!
-end 
+    total = total_food_price + total_drink_price
+    puts " "
+    puts " Your total price is : " + "$"+ total.round(2).to_s
+    puts " " 
+    order_in_progress = false 
+  end
+  
+  if menu_choice == "Exit"
+    order_in_progress = false
+    exit!
+  end   
+end
 
+  ######
